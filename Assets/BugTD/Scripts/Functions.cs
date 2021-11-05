5fad8ceb496a4a883da60cab5df610ec06df08eb
ChungMarcus <75466702+ChungMarcus@users.noreply.github.com>
2021-11-05 23:11:11 +0800

    asdf

--- Assets/BugTD/Scripts/Functions.cs
+++ Assets/BugTD/Scripts/Functions.cs
@@ -24,21 +24,12 @@ public class Functions : MonoBehaviour
     public void StartGame()
     {
         Button.Play();
-        ButtonWaitTime -= Time.deltaTime;
-        if (ButtonStartWaitTime <= 0)
-        {
-            ButtonWaitTime = ButtonStartWaitTime;
-            SceneManager.LoadScene("Level");
-        }
+        SceneManager.LoadScene("Level");
     }
 
     public void LoadMenu()
     {
         Button.Play();
-        ButtonWaitTime -= Time.deltaTime;
-        if (ButtonWaitTime <= 0) {
-            ButtonWaitTime = ButtonStartWaitTime;
-            SceneManager.LoadScene("MainMenu");
-        }
+        SceneManager.LoadScene("MainMenu");
     }
 }
